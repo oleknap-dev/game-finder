dd046d32326f13b128d97c086d46561488c42984
oleknap-dev <aleksander.napiorkowski.an@gmail.com>
2024-12-03 13:28:08 +0100

    Set up project structure and implement initial API data fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,11 @@
-import { useState, useEffect } from "react";
-
-const genre = "rpg";
-const platform = 4; //pc
-
-const API_KEY = "316b02e4cb72464d92ea018d43c541a0";
-const url = `https://api.rawg.io/api/games?key=${API_KEY}&page_size=40`;
+import GameList from "./components/GameList/GameList";
+import Sidebar from "./components/Sidebar/Sidebar";
 
 function App() {
-  const [games, setGames] = useState([]);
-
-  useEffect(() => {
-    async function fetchGames() {
-      let allGames = [];
-      let nextPage = url;
-      let callCounter = 0;
-      try {
-        while (nextPage && callCounter < 2) {
-          const respone = await fetch(nextPage);
-          const data = await respone.json();
-          console.log(data.next);
-          allGames = allGames.concat(data.results);
-          nextPage = data.next;
-          console.log(data.results);
-          callCounter++;
-        }
-        setGames(allGames);
-      } catch (error) {
-        console.log("Error fetching games: ", error);
-      }
-    }
-    fetchGames();
-  }, []);
-
   return (
     <div>
-      <h1>Games:</h1>
-      <ul>
-        {games.map((game) => (
-          <li key={game.id}>
-            <h2>{game.name}</h2>
-            <img src={game.background_image} width={400} height={200} />
-          </li>
-        ))}
-      </ul>
+      <Sidebar />
+      <GameList />
     </div>
   );
 }
