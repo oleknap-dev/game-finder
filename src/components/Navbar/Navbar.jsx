bb5701a1b485aea44525a4e1c81fcc28861293c3
oleknap-dev <aleksander.napiorkowski.an@gmail.com>
2025-01-10 15:41:26 +0100

    Update navbar title from Game Finder to Game Browser

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar() {
     <div className="flex justify-center content-center">
       <div className="bg-green-600 relative my-5 rounded-3xl flex items-center justify-between px-6 w-4/5">
         <div className="py-4 z-10 font-extrabold text-xl text-gray-300 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
-          <Link to="/">Game Finder</Link>
+          <Link to="/">Game Browser</Link>
         </div>
         <ul className="absolute inset-0 text-black flex flex-row m-0 space-x-8 justify-center items-center py-4 font-medium text-base">
           <li>
