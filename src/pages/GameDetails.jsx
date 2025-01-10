5a2250d8679eee58630e266afcf5d549c912b009
oleknap-dev <aleksander.napiorkowski.an@gmail.com>
2025-01-10 13:19:37 +0100

    Update rendering game description with HTML dangerouslySetInnerHTML

--- src/pages/GameDetails.jsx
+++ src/pages/GameDetails.jsx
@@ -5,11 +5,12 @@ function GameDetails() {
   const { id } = useParams();
   const [gameDetails, setGameDetails] = useState();
   const [gameScreenShots, setGameScreenShots] = useState();
+  const [gamePlatforms, setGamePlatforms] = useState();
   const [isLoading, setIsLoading] = useState(true);
 
   const API_KEY = "316b02e4cb72464d92ea018d43c541a0";
   const url = `https://api.rawg.io/api/games/${id}?key=${API_KEY}`;
-  const ScreenShotsUrl = `https://api.rawg.io/api/games/${id}/screenshots?key=${API_KEY}`;
+  const screenShotsUrl = `https://api.rawg.io/api/games/${id}/screenshots?key=${API_KEY}`;
 
   useEffect(() => {
     async function fetchGameDetails() {
@@ -17,7 +18,9 @@ function GameDetails() {
         const respone = await fetch(url);
         const data = await respone.json();
         setGameDetails(data);
-        const repsone2 = await fetch(ScreenShotsUrl);
+        console.log(data.platforms);
+        setGamePlatforms(data.platforms);
+        const repsone2 = await fetch(screenShotsUrl);
         const data2 = await repsone2.json();
         setGameScreenShots(data2.results);
       } catch (error) {
@@ -36,13 +39,17 @@ function GameDetails() {
     <div className="text-white">
       <h1>{gameDetails.name}</h1>
       <h3>About</h3>
-      <p>{gameDetails.description_raw}</p>
-
+      <div dangerouslySetInnerHTML={{ __html: gameDetails.description }}></div>
       <ul>
         {gameScreenShots.map((screenShot) => (
           <li key={screenShot.id}>
             <img src={screenShot.image} width={192} height={108}></img>
           </li>
+        ))}
+      </ul>
+      <ul>
+        {gamePlatforms.map((object) => (
+          <li>{console.log(object.requirements.minimum)}</li>
         ))}
       </ul>
     </div>
